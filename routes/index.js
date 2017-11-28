f4139daeb0cf50eb2b5559de546422131c108154
Jens Tuyls <you@example.com>
2017-11-28 13:51:53 -0800

    commented code

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ router.get('/r/:key', function(req, res, next) {
     if (err) throw err;
     if (!board) {
 
+      // make a matrix that represents the colors of every position
       let colorMatrix = []
       for (var i = 0; i < 5; ++i) {
         colorMatrix.push([])
@@ -18,6 +19,7 @@ router.get('/r/:key', function(req, res, next) {
         }
       }
 
+      // create the board, rows and columns were hardcoded to 5
       let board = new Board({
         key: req.params.key,
         colors: colorMatrix,
@@ -25,6 +27,7 @@ router.get('/r/:key', function(req, res, next) {
         row: 5
       }); 
     
+      // save the the board information to the database 
       board.save(function(err, newBoard) {
         if (err) throw err;  
         res.render('index', { title: 'Color Picker', board: newBoard });
@@ -37,9 +40,12 @@ router.get('/r/:key', function(req, res, next) {
 });
 
 router.patch('/colors', function(req, res, next) {
+  // find the board, then update the colorMatrix
   Board.findOne({ "key": req.body.boardKey }, function(err, currentBoard) {
     let colorMatrix = currentBoard.colors;
     colorMatrix[req.body.row][req.body.col] = req.body.color; 
+    
+    // find the board by id and update the colorMatrix
     Board.findByIdAndUpdate({ _id: currentBoard._id }, { colors: colorMatrix }, { new: true }, function(err, newBoard) {
       socketio.instance().sockets.in(req.body.boardKey).emit('color_change', [req.body.row, req.body.col, req.body.color]);  
       res.end(); 
@@ -47,6 +53,7 @@ router.patch('/colors', function(req, res, next) {
   });
 });
 
+// get the current colorMatrix for the board 
 router.get('/colors/reset/:key', function(req, res, next) {
   Board.findOne({ "key": req.params.key }, function(err, board) {
     res.json(board.colors);  
